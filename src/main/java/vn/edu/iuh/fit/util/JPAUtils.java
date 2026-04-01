1de3cb3e96dded0e1b591b97fca9c3ed93094abf
Phạm Trà <phamvantra.dev@gmail.com>
2026-04-01 23:04:54 +0700

    chore: initialize project

---
+++ src/main/java/vn/edu/iuh/fit/util/JPAUtils.java
@@ -0,0 +1,28 @@
+package vn.edu.iuh.fit.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtils {
+
+    private static final String PERSISTENCE_UNIT_NAME = "mariadb-pu";
+    private static EntityManagerFactory factory;
+
+    static {
+        try {
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to initialize EntityManagerFactory: " + e.getMessage());
+        }
+    }
+
+    public static EntityManagerFactory getFactory() {
+        return factory;
+    }
+
+    public static EntityManager getEntityManager() {
+        return factory.createEntityManager();
+    }
+}
