cff6eaf08221743962901c6333deba594ba520f5
Phạm Thanh Huy <huyp7922@gmail.com>
2026-04-15 08:19:41 +0700

    fix: correct import for JPAUtils package

--- src/main/java/vn/edu/iuh/fit/client/Main.java
+++ src/main/java/vn/edu/iuh/fit/client/Main.java
@@ -1,6 +1,7 @@
 package vn.edu.iuh.fit.client;
 
-import vn.edu.iuh.fit.util.JPAUtils;
+
+import vn.edu.iuh.fit.server.util.JPAUtils;
 
 public class Main {
     public static void main(String[] args) {
